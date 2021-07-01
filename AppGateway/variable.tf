08ac553c2aec6cb774b30fd22930b322334900b0
jye112 <50107548+jye112@users.noreply.github.com>
2021-07-01 15:30:03 +0900

    Update variable.tf

--- AppGateway/variable.tf
+++ AppGateway/variable.tf
@@ -22,6 +22,6 @@ variable hw_appgw {
 
 variable hw_appgw_backend {
     type = string
-    default = "/subscriptions/0043bf9b-ebf8-46ce-9c8e-685cebce15b1/resourceGroups/hwsc-d-krc-dt-rg/providers/Microsoft.Network/applicationGateways/hwsc-d-krc-agw-001/backendAddressPools/t-terraform-f-be-001"
+    default = ""
 }
 
