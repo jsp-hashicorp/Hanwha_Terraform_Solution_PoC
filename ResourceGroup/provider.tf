d158a67ebbee0f2b54c4b99db4dacdb61d2813a6
jye112 <50107548+jye112@users.noreply.github.com>
2021-07-01 15:30:44 +0900

    Update provider.tf

--- ResourceGroup/provider.tf
+++ ResourceGroup/provider.tf
@@ -8,27 +8,6 @@ terraform {
   }
 }
 
-#variable azure_subscription {
-#    type = string
-#    default = "c7107052-a041-47b9-bd39-682496e017ab"
-#}
-#
-#variable azure_tenantID {
-#    type = string
-#    default = "a74b5451-a280-4768-a5cd-cb47af153c71"
-#}
-#
-#variable azure_clientID {
-#    type = string
-#    default = "da9e40da-c7af-4139-9fed-1fe21399eb60"
-#}
-#
-#variable azure_client_secret {
-#    type = string
-#    default = "EZYtex_OLYC9_Ty0yiEqh2c4P9LV_o3HS~"
-#}
-
-
 provider "azurerm" {
     version = "~>2.0"
     
