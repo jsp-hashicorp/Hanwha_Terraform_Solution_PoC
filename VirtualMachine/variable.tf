47b645a3984ae85644ee599b91131302e3778591
JS Park <jsp@hashicorp.com>
2021-07-02 11:23:11 +0900

    Update variable.tf

--- VirtualMachine/variable.tf
+++ VirtualMachine/variable.tf
@@ -12,6 +12,12 @@ variable "cloo_rg" {
 variable "location" {
   type    = string
   default = "koreacentral"
+  description = "배포 지역 설정 한국 중부(koreacentral), 한국 남부(koreasouth)"
+  
+  validation {
+    condition = contains(["koreacentral", "Korea Central", "koreasouth", "Korea South"], var.location)
+    error_message = "Error : \n 지원되지 않는 배포 지역입니다. \n 다음 배포 지역(Region)만 사용 가능합니다. \n \t - 한국 중부 : koreacentral , Korea Central \n \t - 한국 남부 : koreasouth, Korea South \n ."
+  }
 }
 
 
@@ -62,6 +68,10 @@ variable "cloo_vm_02" {
 variable "cloo_vm_size" {
   type    = string
   default = "Standard_D2s_v3"
+   validation {
+    condition = contains(["Standard_D4s_v3" , "Standard_D2s_v3" , "Standard_D3_v2", "Standard_D16s_v3", "Standard_E8s_v3", "Standard_F4s"], var.aks_vm_size)
+    error_message = "Error : \n 지원되지 않는 VM Size입니다. \n 다음 VM Size만 사용 가능합니다. \n \t - Standard_D4s_v3 \n \t - Standard_D2s_v3 \n \t - Standard_D3_v2\n \t - Standard_D16s_v3\n \t - Standard_E8s_v3 \n \t - Standard_F4s \n ."
+  }
 }
 
 variable "cloo_vm_admin_username" {
